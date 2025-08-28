55c0872daa4b7f9dae880c755b2e4535e737a453
Mr.Igor <iv_mirsoft@ukr.net>
2025-08-28 23:34:38 +0300

    modal fixes

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,70 +1,25 @@
-"use client"
+import NotePreviewClient from "./NotePreview.client"
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query"
 
-import { useQuery } from "@tanstack/react-query"
-import { useParams, useSearchParams } from "next/navigation"
-import { getSingleNote } from "@/lib/api"
-import css from "./NotePreview.module.css"
-import Loading from "@/app/loading"
-import Error from "@/app/error"
-import ModalNote from "@/components/ModalNote/ModalNote"
+type Props = {
+	params: Promise<{ id: string }>
+}
 
-const NotePreview = () => {
-	const { id } = useParams<{ id: string }>()
+const Page = async ({ params }: Props) => {
+	const { id } = await params
 
-	const searchParams = useSearchParams()
-	const from = searchParams.get("from") // твій slug
+	//const queryClient = new QueryClient()
 
-	//console.log("NotePreview", id)
-	const {
-		data: note,
-		isLoading,
-		error,
-	} = useQuery({
-		queryKey: ["notesQuery", id],
-		queryFn: () => getSingleNote(id),
-		refetchOnMount: false,
-	})
-
-	if (isLoading) return <Loading />
-
-	if (error || !note) return <Error error={error ? error : undefined} />
-
-	const dateFormat: Intl.DateTimeFormatOptions = {
-		day: "2-digit",
-		month: "long",
-		year: "numeric",
-		hour: "2-digit",
-		minute: "2-digit",
-	}
-
-	const isValidDate = (value: string): boolean => {
-		const d = new Date(value)
-		return !isNaN(d.getTime())
-	}
-
-	const formatDateSafe = (value: string): string => {
-		return isValidDate(value) ? new Date(value).toLocaleDateString("en-US", dateFormat) : value
-	}
-
-	const createdAt = formatDateSafe(note.createdAt)
-	const updatedAt = formatDateSafe(note.updatedAt)
-
-	const formattedDate = note.updatedAt ? `Updated at: ${updatedAt}` : `Created at: ${createdAt}`
+	//await queryClient.prefetchQuery({
+	//	queryKey: ["note", id],
+	//	queryFn: () => fetchNoteById(id),
+	//})
 
 	return (
-		<ModalNote backPage={from ? from : "All notes"}>
-			<div className={css.container}>
-				<div className={css.item}>
-					<div className={css.header}>
-						<h2>{note.title}</h2>
-					</div>
-					<p className={css.description}>{note.content}</p>
-					<p className={css.date}>{formattedDate}</p>
-					<p className={css.tag}>{note.tag}</p>
-				</div>
-			</div>
-		</ModalNote>
+		//<HydrationBoundary state={dehydrate(queryClient)}>
+		<NotePreviewClient />
+		//</HydrationBoundary>
 	)
 }
 
-export default NotePreview
+export default Page
