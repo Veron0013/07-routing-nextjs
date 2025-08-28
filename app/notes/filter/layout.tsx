c8b70dc2bfe9970ed608e586b8ecfb1bfb05bc80
Mr.Igor <iv_mirsoft@ukr.net>
2025-08-28 22:59:40 +0300

    menu fixes2

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,5 +1,5 @@
-import { Suspense } from "react"
-import Loading from "../../loading"
+//import { Suspense } from "react"
+//import Loading from "../../loading"
 
 //export default async function NotesLayout({
 export default function NotesLayout({
